5a8d6dcee6ec259bb01e7113563837189e7e40a1
berlinpaige <berlinsohn@gmail.com>
2016-10-18 22:01:26 -0600

    improved mobile

--- source/js/video.js
+++ source/js/video.js
@@ -25,14 +25,25 @@ window.requestAnimFrame = (function(){ // reduce CPU consumption, improve perfor
 $(document).scroll(function(){
   
     (function animloop(){ // the smoothest animation loop possible
-      if ($(window).scrollTop() > $('.main-content').height() - 300) {
-        requestAnimFrame(animloop);
+      if (window.innerWidth > 736) {
+        if ($(window).scrollTop() > $('.main-content').height() - 300) {
+          requestAnimFrame(animloop);
+        }
+        else if ($(window).scrollTop() < $('.main-content').height() - 300) {
+          window.cancelAnimationFrame(animloop)
+        }
+        var videoTop = $('.main-content').height() - 300;
       }
-      else if ($(window).scrollTop() < $('.main-content').height() - 300) {
-        window.cancelAnimationFrame(animloop)
+      else {
+        if ($(window).scrollTop() > $('.main-content').height() + 900) {
+          requestAnimFrame(animloop);
+        }
+        else if ($(window).scrollTop() < $('.main-content').height() + 900) {
+          window.cancelAnimationFrame(animloop)
+        }
+        var videoTop = $('.main-content').height() + 900;
       }
 
-      var videoTop = $('.main-content').height() - 300;
       targetStep = Math.max( Math.round( ($(window).scrollTop() - videoTop) / 30 ) , 1 ); // what frame to animate to
       if(targetStep != step ) { step += (targetStep - step) / 5; } // increment the step until we arrive at the target step
       changeFrame();
@@ -51,8 +62,8 @@ function changeFrame() {
 }
 
 function resizeAdjustments() { // fit everything to the screen
-  var windowWidth = window.innerWidth, //yes I'm using this
-      windowHeight = window.innerHeight,  //yes, I'm using this
+  var windowWidth = window.innerWidth,
+      windowHeight = window.innerHeight, 
       scrollExtraHeight = 30 * totalFrames,
       mainContentHeight = $('.main-content').height(),
       videoTop = $('.main-content').height() - 300,
@@ -62,20 +73,20 @@ function resizeAdjustments() { // fit everything to the screen
 
   $('.video--container').css({"height": videoHeight + 'px', "width": windowWidth}); 
   $('.video--wrapper').css({"padding-top": ((videoHeight/windowWidth)*100) + '%'});
-  $('.scroll-extra-height').css({"height": scrollExtraHeight + videoHeight + 'px'});
-
+  
   if (imageWidth/imageHeight > window.innerWidth/videoHeight) {
     $('#video').css({"height": videoHeight + 'px', "width": "auto"});
   }
   else {
     $('#video').css({"height": "auto", "width": window.innerWidth}); 
   }
+    $('.video--container').css({'padding-top': windowHeight - 395, 'margin-top': -(windowHeight - 395) -18})
 
   if (window.innerWidth > 736) {
+    $('.scroll-extra-height').css({"height": scrollExtraHeight + videoHeight + 'px'});
 
     $('.section--heading').css({'width': windowWidth/2, 'height': windowHeight, 'line-height': '699px'}) 
     $('.section').css({'min-height': windowHeight})
-    $('.video--container').css({'padding-top': windowHeight - 395, 'margin-top': -(windowHeight - 395) -18})
  
 
     $(document).scroll(function() {
@@ -84,74 +95,90 @@ function resizeAdjustments() { // fit everything to the screen
         $aboutTop = $('.about').offset().top,
         $contactTop = $('.contact').offset().top,
         $videoContainerTop = $('.video--container').offset().top,
-        $work = $('.heading--work'),
-        $about = $('.heading--about'),
-        $contact = $('.heading--contact'),
+        $work = $('.work'),
+        $about = $('.about'),
+        $contact = $('.contact'),
         $sectionHeading = $('.section--heading'),
-        $videoContain = $('.video--container'); 
+        $videoContain = $('.video--container'),
+        scrollDownDistance = $('.main-content').height() - $('.content-contact').height() -300;
 
-        // console.log($scrollTop)
-        // console.log($('.main-content').height())
+        console.log('$scrollTop', $scrollTop)
+        console.log('$videoContainerTop', $videoContainerTop)
 
     if($scrollTop < $workTop) {
-      $sectionHeading.removeClass('fixed')
+      // $sectionHeading.removeClass('fixed');
+      $work.removeClass('fixed');
     }    
 
     if($scrollTop > $workTop && $scrollTop < $aboutTop - windowHeight) {
-      $work.addClass('fixed')
-      $work.removeClass('absolute--bottom')
+      $work.addClass('fixed');
+      $work.removeClass('absolute--bottom');
     }
 
     if ($scrollTop > $aboutTop - windowHeight) {
-      $about.removeClass('fixed')
-      $work.removeClass('fixed')
-      $work.addClass('absolute--bottom')
+      $about.removeClass('fixed');
+      $work.removeClass('fixed');
+      $work.addClass('absolute--bottom');
     }
 
     if ($scrollTop > $aboutTop && $scrollTop< $contactTop - windowHeight) {
-      $about.removeClass('absolute--bottom')
-      $about.addClass('fixed')
+      $about.removeClass('absolute--bottom');
+      $about.addClass('fixed');
     }
 
     if ($scrollTop > $contactTop - windowHeight) {
-      $contact.removeClass('fixed')
-      $about.removeClass('fixed')
-      $about.addClass('absolute--bottom')
+      $contact.removeClass('fixed');
+      $about.removeClass('fixed');
+      $about.addClass('absolute--bottom');
     }
 
     if ($scrollTop > $contactTop) {
-      $contact.removeClass('absolute--bottom')
-      $contact.addClass('fixed')
+      $contact.removeClass('absolute--bottom');
+      $contact.addClass('fixed');
     }
 
     if ($scrollTop > $videoContainerTop -300) {
-      console.log('hit it')
-      $contact.removeClass('fixed')
-      $contact.addClass('absolute--bottom')
+      $contact.removeClass('fixed');
+      $contact.addClass('absolute--bottom');
     }
 
-    console.log($scrollTop)
-    console.log($videoContainerTop)
-
-    if ($scrollTop < 4228) {
-      console.log('less than')
-      $videoContain.removeClass('fixed')
-      $contact.removeClass('fixedForVideo')
+    if ($scrollTop < scrollDownDistance) {
+      $videoContain.removeClass('fixed');
+      $contact.removeClass('fixedForVideo');
     }
 
     if ($scrollTop > $videoContainerTop) {
-      $videoContain.addClass('fixed')
-      $contact.addClass('fixedForVideo')
-
-      // if ($scrollTop < 4138) {
-      //   console.log('less than')
-      //   $videoContain.removeClass('fixed')
-      //   $contact.removeClass('fixedForVideo')
-      // }
+      $videoContain.addClass('fixed');
+      $contact.addClass('fixedForVideo');
     }
 
   });
 
+  }
+  else {
+    $('.scroll-extra-height').css({"height": scrollExtraHeight + videoHeight*4 + 'px'});
+
+    $('.section--heading').css({'width': 'auto', 'height': 'auto', 'line-height': 'inherit'}) 
+    $('.section').css({'min-height': 'auto'})
+
+    $(document).scroll(function() {
+
+      var $scrollTop = $(window).scrollTop(),
+        $videoContainerTop = $('.video--container').offset().top,
+        $videoContain = $('.video--container'),
+        $contactContent = $('.content--contact'),
+        scrollDownDistance = $('.main-content').height() - $('.content-contact').height() + 837
+
+       if ($scrollTop < scrollDownDistance) {
+          $videoContain.removeClass('fixed');
+          $contactContent.removeClass('fixedForVideo');
+        }
+
+        if ($scrollTop >= $videoContainerTop) {
+          $videoContain.addClass('fixed');
+          $contactContent.addClass('fixedForVideo');
+        }
+    });
   }
 }
 
